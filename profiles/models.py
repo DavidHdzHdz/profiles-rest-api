dada7d4f6f8654c7b0e111a0d98cad0c59219934
DavidHdzHdz <david.hernandez.hernandez.contractor@bbva.com>
2019-09-05 18:15:15 -0500

    virtualized and app setup started

---
+++ profiles/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """
+    Manager for user profiles
+    """
+    def _create_user(self, name, email, password, **extra_fields):
+        """ Create and save a user with the given username, email, and password. """
+        if not email:
+            raise ValueError('User must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(name=name, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, name, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(name, email, password, **extra_fields)
+
+    def create_superuser(self, name, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(name, email, password, **extra_fields)
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    Database model for users in the system
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """ Retrieve full name of user """
+        return self.name
+
+    def get_short_name(self):
+        """ Retrieve short name of user """
+        return self.name
+
+    def __str__(self):
+        """ Return string representation of our user """
+        return self.email
