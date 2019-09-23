e97097d44aa8c3d6e4cfd0469e9110ca7d171f89
DavidHdzHdz <david.hernandez.hernandez.contractor@bbva.com>
2019-09-23 18:13:48 -0500

    update allowed hosts

--- profiles_rest_api/settings.py
+++ profiles_rest_api/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '2$u123%q&)4sg)c-dk=7q&m(wfp8l_+hej&uhr=565(s2(u**g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://ec2-54-187-205-38.us-west-2.compute.amazonaws.com/',
+    '127.0.0.1'
+]
 
 
 # Application definition
